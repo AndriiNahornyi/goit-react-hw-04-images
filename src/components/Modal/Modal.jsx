8dd9ec0ddb2d63c7c98d0174a3be519f0c66a92c
Andrii Nahornyi <108332521+AndriiNahornyi@users.noreply.github.com>
2022-10-28 20:23:48 +0300

    adds Modal & App solution

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,36 +1,41 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import css from './Modal.module.css';
 
-export class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener('keydown', this.keyClose);
-  }
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.keyClose);
-  }
-  keyClose = e => {
+export const Modal = ({ largeImageURL, onClose }) => {
+  useEffect(() => {
+    document.addEventListener('keydown', keyClose);
+    return () => {
+      document.removeEventListener('keydown', keyClose);
+    };
+    // eslint-disable-next-line
+  }, []);
+
+  // componentDidMount() {
+  //   document.addEventListener('keydown', this.keyClose);
+  // }
+  // componentWillUnmount() {
+  //   document.removeEventListener('keydown', this.keyClose);
+  // }
+
+  const keyClose = e => {
     if (e.key === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
-  overlayClick = e => {
+  const overlayClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      onClose();
     }
   };
-
-  render() {
-    return (
-      <div onClick={this.overlayClick} className={css.Overlay}>
-        <div className={css.Modal}>
-          <img src={this.props.largeImageURL} alt="" />
-        </div>
+  return (
+    <div onClick={overlayClick} className={css.Overlay}>
+      <div className={css.Modal}>
+        <img src={largeImageURL} alt="" />
       </div>
-    );
-  }
-}
-
+    </div>
+  );
+};
 Modal.propTypes = {
   largeImageURL: PropTypes.string.isRequired,
 };
