184c575654a29a0c703229762dd6b3921ca5dcb2
Andrii Nahornyi <108332521+AndriiNahornyi@users.noreply.github.com>
2022-10-29 19:00:35 +0300

    update proptypes

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,9 +1,11 @@
 import { Button } from '../Button';
 import { Loader } from '../Loader';
 import { ImageGalleryItem } from '../ImageGalleryItem';
+import PropTypes from 'prop-types';
 import css from './ImageGallery.module.css';
 
 export const ImageGallery = ({ images, toggleModal, isLoading, loadMore }) => {
+  console.log('images :>> ', images);
   return (
     <>
       {images.length > 0 && (
@@ -22,4 +24,14 @@ export const ImageGallery = ({ images, toggleModal, isLoading, loadMore }) => {
       <Button loadMore={loadMore} />
     </>
   );
+};
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+    })
+  ),
+  toggleModal: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  loadMore: PropTypes.func.isRequired,
 };
